c0ef46c8f76164ac3dedeb4f11bd9d4adfc62cd7
Aneesh Dalvi <adalvi@idirect.net>
2015-05-02 23:02:56 -0400

    use solarized for pygmentize; update pip install script

--- pip_stuff.sh
+++ pip_stuff.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-pip install --upgrade pygments
+if ( ! which pip ); then
+	sudo easy_install pip
+fi
+
+sudo pip install --upgrade pygments
+sudo pip install --upgrade pygments-style-solarized
+
